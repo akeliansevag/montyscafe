6b4462e9db80c3832a1443aa372df69e80d60779
Hassan Jammal <hassan-jammal@hotmail.com>
2023-10-27 12:11:14 +0300

    fix

--- page-private-events.php
+++ page-private-events.php
@@ -10,60 +10,60 @@
                 <div class="col-12 col-lg-6 px-0 pe-lg-6">
                     <h1 class="fs-3 primary-color">Book your private event in seconds</h1>
                     <div class="d-flex flex-column gap-4 mt-5">
+                        <form action="" method="POST">
+                            <div class="d-flex flex-column flex-lg-row justify-content-between gap-4">
+                                <div class="w-100">
+                                    <label for="first-name" class="d-block fs-9">First Name</label>
+                                    <input type="text" name="first-name" id="first-name" class="mt-2 w-100 p-2">
+                                </div>
 
-                        <div class="d-flex flex-column flex-lg-row justify-content-between gap-4">
-                            <div class="w-100">
-                                <label for="first-name" class="d-block fs-9">First Name</label>
-                                <input type="text" name="first-name" id="first-name" class="mt-2 w-100 p-2">
+                                <div class="w-100">
+                                    <label for="last-name" class="d-block fs-9">Last Name</label>
+                                    <input type="text" name="last-name" id="last-name" class="mt-2 w-100 p-2">
+                                </div>
                             </div>
 
-                            <div class="w-100">
-                                <label for="last-name" class="d-block fs-9">Last Name</label>
-                                <input type="text" name="last-name" id="last-name" class="mt-2 w-100 p-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="w-100">
+                                    <label for="email" class="d-block fs-9">Email</label>
+                                    <input type="text" name="email" id="email" class="mt-2 w-100 p-2">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="d-flex justify-content-between">
-                            <div class="w-100">
-                                <label for="email" class="d-block fs-9">Email</label>
-                                <input type="text" name="email" id="email" class="mt-2 w-100 p-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="w-100">
+                                    <label for="phone" class="d-block fs-9">Phone Number</label>
+                                    <input type="tel" name="phone" id="phone" class="mt-2 w-100 p-2">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="d-flex justify-content-between">
-                            <div class="w-100">
-                                <label for="phone" class="d-block fs-9">Phone Number</label>
-                                <input type="tel" name="phone" id="phone" class="mt-2 w-100 p-2">
+                            <div class="d-flex justify-content-between">
+                                <div class="w-100">
+                                    <label for="date" class="d-block fs-9">Event date</label>
+                                    <input type="date" name="date" id="date" class="mt-2 w-100 p-2" placeholder="dd-mm-yyyy">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="d-flex justify-content-between">
-                            <div class="w-100">
-                                <label for="date" class="d-block fs-9">Event date</label>
-                                <input type="date" name="date" id="date" class="mt-2 w-100 p-2" placeholder="dd-mm-yyyy">
+                            <div class="d-flex justify-content-between">
+                                <div class="w-100">
+                                    <label for="invitees" class="d-block fs-9">Number of invitees</label>
+                                    <input type="text" name="invitees" id="invitees" class="mt-2 w-100 p-2">
+                                </div>
                             </div>
-                        </div>
+                            
+                            <div class="d-flex justify-content-between">
+                                <div class="w-100 containerr" >
+                                    <input type="checkbox" name="acknowledgement" id="ack-label">       
+                                    <label for="ack-label">You agree to our <a id="acknowledgement" href="">privacy policy</a>.</label>
+                                </div>
+                            </div>
 
-                        <div class="d-flex justify-content-between">
-                            <div class="w-100">
-                                <label for="invitees" class="d-block fs-9">Number of invitees</label>
-                                <input type="text" name="invitees" id="invitees" class="mt-2 w-100 p-2">
+                            <div class="d-flex justify-content-between mt-3">
+                                <div class="w-100">
+                                    <button class="button-secondary py-3 px-5 w-100 rounded-pill">Request Reservation</button>
+                                </div>
                             </div>
-                        </div>
-                        
-                        <div class="d-flex justify-content-between">
-                            <div class="w-100 containerr" >
-                                <input type="checkbox" name="acknowledgement" id="ack-label">       
-                                <label for="ack-label">You agree to our <a id="acknowledgement" href="">privacy policy</a>.</label>
-                            </div>
-                        </div>
-
-                        <div class="d-flex justify-content-between mt-3">
-                            <div class="w-100">
-                                <button class="button-secondary py-3 px-5 w-100 rounded-pill">Request Reservation</button>
-                            </div>
-                        </div>
-
+                        </form>
                     </div>
                 </div>
                 <div class="col-12 col-lg-6 d-none d-lg-flex justify-content-end">
